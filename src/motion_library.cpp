45a59b3c7cba28de32ccb21f1f370a8567ef37df
FLA-11 <fla@fla-11.local>
2017-02-15 17:47:53 -0500

    Adjust buffer.

--- src/motion_library.cpp
+++ src/motion_library.cpp
@@ -106,7 +106,7 @@ size_t MotionLibrary::getNumMotions() {
 
 Vector3 MotionLibrary::getSigmaAtTime(double const& t) {
 	//return Vector3(0.01,0.01,0.01) + t*0.2*(Vector3(0.5,0.5,0.5) + 0.5*(initial_velocity.array().abs()).matrix());
-	return Vector3(0.01,0.01,0.01) + t*(Vector3(0.5,0.5,0.4) + 0.1*Vector3(1.0,1.0,0.0)*(initial_velocity.norm()) );
+	return Vector3(0.1,0.1,0.01) + t*(Vector3(0.5,0.5,0.4) + 0.1*Vector3(1.0,1.0,0.0)*(initial_velocity.norm()) );
 };
 
 Vector3 MotionLibrary::getLASERSigmaAtTime(double const& t) {
